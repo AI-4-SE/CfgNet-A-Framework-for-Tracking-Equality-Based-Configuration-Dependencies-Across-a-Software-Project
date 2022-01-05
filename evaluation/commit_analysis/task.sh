b0fac711601bf9d256678c98fdf3daf405cc251b
Nicolai Ruckel <nicolai@ruckel.de>
2022-01-05 12:13:38 +0100

    Slurm magic

--- evaluation/commit_analysis/task.sh
+++ evaluation/commit_analysis/task.sh
@@ -1,13 +1,13 @@
 EVALUATION=out
 
-if [[ $(hostname -s) = brown* ]]; then
-    LOCALPATH=/tmp/$USER/analysis/$1
-    rm -rf "$LOCALPATH"
-    mkdir "$LOCALPATH"
-else
-    LOCALPATH=.
-    rm -rf "$LOCALPATH"/venv
-fi
+# if [[ $(hostname -s) = brown* ]]; then
+LOCALPATH=/tmp/$USER/analysis/$1
+    # rm -rf "$LOCALPATH"
+    # mkdir "$LOCALPATH"
+# else
+    # LOCALPATH=.
+    # rm -rf "$LOCALPATH"/venv
+# fi
 
 python3 -m venv $LOCALPATH/venv
 source $LOCALPATH/venv/bin/activate
@@ -25,17 +25,18 @@ rm -rf "$EVALUATION"
 echo "======================="
 echo "Start commit history analysis!"
 
-python3 $2/evaluation.py
+cp $2/evaluation.py .
+python3 evaluation.py $1
 
 echo "======================="
 echo "Commit History Analysis done!"
 
-cp -r out/results/* $2/results
+# cp -r out/results/* $2/results
+cp -r $LOCALPATH/out/* $2/results/
 
 deactivate
-
-if [[ $(hostname -s) = brown* ]]; then
+# if [[ $(hostname -s) = brown* ]]; then
     rm -rf "$LOCALPATH"
-else
-    rm "$wheel"
-fi
+# else
+#     rm "$wheel"
+# fi
