3fe76096d9b0a53c9fce3284e518857e817c6509
Sebastian Simon <Bastisimon95@googlemail.com>
2023-03-01 11:37:19 +0100

    Compute agreement for snowballing sample

--- src/literature_study.py
+++ src/literature_study.py
@@ -9,10 +9,10 @@ def create_snowballing_sample():
     df_not_found = df[df["Annotator 1"] == "not found"]
     df_not_accessible= df[df["Annotator 1"] == "not accessible"]
 
-    assert sum([len(df_not_relevant), len(df_not_found), len(df_not_accessible), len(df_relevant)]) == 948
+    assert sum([len(df_not_relevant), len(df_not_found), len(df_not_accessible), len(df_relevant)]) == 943
     
-    print("Length relevant: ", len(df_relevant)) # len = 207 20% == 
-    print("Length not-relevant: ", len(df_not_relevant))
+    print("Length relevant: ", len(df_relevant)) # 20% von 82 => 16
+    print("Length not-relevant: ", len(df_not_relevant)) # 20% von 861 = 172
 
     df_relevant_sample = df_relevant.sample(41)
     df_not_relevant_sample = df_not_relevant.sample(143)
@@ -33,24 +33,32 @@ def calculate_annotator_agreement_initial_set():
     relevance_annotator_2_revised = df_revised["Annotator 2"].tolist()
 
     agreement = cohen_kappa_score(relevance_annotator_1, relevance_annotator_2)
-    agreement_revsied = cohen_kappa_score(relevance_annotator_1_revised, relevance_annotator_2_revised)
+    agreement_revised = cohen_kappa_score(relevance_annotator_1_revised, relevance_annotator_2_revised)
 
-    print("Annotator Agreement with intial inclusion/exlcusion criteria: ", agreement)
-    print("Annotator Agreement with revsied inclusion/exclusion criteria: ", agreement_revsied)
+    print("Annotator Agreement with initial inclusion/exclusion criteria: ", agreement)
+    print("Annotator Agreement with revised inclusion/exclusion criteria: ", agreement_revised)
 
 
-def calculate_annotatore_agreement_snowballing_sample():
-    pass
+def calculate_annotator_agreement_snowballing_sample():
+    df = pd.read_csv("../data/literature_study/snowballing_sample.csv")
+
+    relevance_annotator_1 = df["Annotator 1"].tolist()
+    relevance_annotator_2 = df["Annotator 2"].tolist()
+
+    agreement = cohen_kappa_score(relevance_annotator_1, relevance_annotator_2)
+
+    print("Annotator Agreement for snowballing sample : ", agreement)
 
 
 def create_final_set():
     df = pd.read_csv("../data/literature_study/snowballing.csv")
     df_final = df[df["Annotator 1"] == "relevant"]
-
     df_final.to_csv("../data/literature_study/final_set.csv", index=False)
 
 
 if __name__ == "__main__":
     #calculate_annotator_agreement_initial_set()  
     #create_snowballing_sample()
-    create_final_set()
+    #create_final_set()
+    calculate_annotator_agreement_snowballing_sample()
+    #pass
